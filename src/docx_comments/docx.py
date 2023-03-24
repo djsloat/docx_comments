c0576ca03e0e8a96c57e0c363a748c71135ff255
djsloat <43764827+djsloat@users.noreply.github.com>
2023-03-24 15:50:00 -0700

    Initial commit

---
+++ src/docx_comments/docx.py
@@ -0,0 +1,43 @@
+"""Class Document"""
+
+from functools import cached_property
+from pathlib import Path
+from zipfile import ZipFile
+
+from lxml import etree
+
+from docx_comments.comments.comments import Comments
+from docx_comments.notes.notes import Notes
+from docx_comments.styles.styles import Styles
+from docx_comments.logger import log_filename
+
+
+@log_filename
+class Document:
+    """Opens docx document and creates XML file tree"""
+
+    def __init__(self, filename):
+        self.file = Path(filename)
+        self.styles = Styles(self)
+        self.notes = Notes(self)
+        self.comments = Comments(self)
+
+    def __repr__(self):
+        return f"Document(file='{self.file}')"
+
+    @cached_property
+    def xml(self):
+        with ZipFile(self.file, "r") as z:
+            return {
+                filename: etree.fromstring(z.read(filename))
+                for filename in z.namelist()
+                if filename
+                in (
+                    "word/document.xml",
+                    "word/styles.xml",
+                    "word/comments.xml",
+                    "word/commentsExtended.xml",
+                    "word/footnotes.xml",
+                    "word/endnotes.xml",
+                )
+            }
---
+++ src/docx_comments/elements/bubble.py
@@ -0,0 +1,9 @@
+from docx_comments.elements.paragraph import Paragraph
+from docx_comments.elements.paragraph_group import ParagraphGroup
+
+
+class Bubble(ParagraphGroup):
+    def __init__(self, paragraphs, document):
+        self._doc = document
+        self.paragraphs = [Paragraph(el, self._doc) for el in paragraphs]
+        super().__init__(self.paragraphs)
