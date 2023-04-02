eda158ec1fcf6662e8d47b44bcda3f58642d73b1
djsloat <43764827+djsloat@users.noreply.github.com>
2023-04-02 10:12:21 -0700

    Fix typing issue

--- src/docx_comments/elements/bubble.py
+++ src/docx_comments/elements/bubble.py
@@ -1,16 +1,20 @@
 """Comment bubble."""
 
+from typing import TYPE_CHECKING
+
 from lxml.etree import _Element
 
-from docx_comments.docx import Document
 from docx_comments.elements.paragraph import Paragraph
 from docx_comments.elements.paragraph_group import ParagraphGroup
+
+if TYPE_CHECKING:
+    from docx_comments.docx import Document
 
 
 class Bubble(ParagraphGroup):
     """Comment bubble."""
 
-    def __init__(self, paragraphs: _Element, document: Document):
+    def __init__(self, paragraphs: _Element, document: "Document"):
         self._doc = document
         self.paragraphs = [Paragraph(el, self._doc) for el in paragraphs]
         super().__init__(self.paragraphs)
