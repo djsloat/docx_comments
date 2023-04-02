e98c2727863aab04faec89a97d20d73c17574e8d
djsloat <43764827+djsloat@users.noreply.github.com>
2023-04-01 22:07:43 -0700

    Add typing

--- src/docx_comments/elements/properties.py
+++ src/docx_comments/elements/properties.py
@@ -12,18 +12,18 @@ class Properties:
         self._parent = run
         self._element = run.element
         self._styles = self._parent._parent._doc.styles.styles
-        self.rstyle = self._element.xpath("string(w:rPr/w:rStyle/@w:val)", **ns)
+        self.rstyle: str = self._element.xpath("string(w:rPr/w:rStyle/@w:val)", **ns)
 
     @cached_property
-    def pstyle(self):
+    def pstyle(self) -> str:
         return self._element.xpath("string(parent::w:p/w:pPr/w:pStyle/@w:val)", **ns)
 
     @cached_property
-    def chain(self):
+    def chain(self) -> ChainMap:
         rstyle_run_props = self._styles[self.rstyle].run if self.rstyle else {}
         pstyle_run_props = self._styles[self.pstyle].run if self.pstyle else {}
         return ChainMap(self._parent._props, rstyle_run_props, pstyle_run_props)
 
     @property
-    def decode(self):
+    def decode(self) -> PropDecode:
         return PropDecode(self.chain)
